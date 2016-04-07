ee17503c77338dda61af83e6f53149cfdba140c4
Anton <380685545353@ya.ru>
2016-04-07 11:26:11 +0300

    Created 5 circles. Rework slow motion. Add ic-launcher.

--- app/src/main/java/com/example/i7/changescenecircle/LayoutActivity.java
+++ app/src/main/java/com/example/i7/changescenecircle/LayoutActivity.java
@@ -1,18 +1,20 @@
 package com.example.i7.changescenecircle;
 // http://learn-android.ru/news/osnovy_raboty_s_perekhodami/2015-01-14-13
+// http://habrahabr.ru/post/243363/
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AccelerateDecelerateInterpolator;
-import android.widget.RelativeLayout;
-
 import com.transitionseverywhere.AutoTransition;
+import com.transitionseverywhere.ChangeBounds;
 import com.transitionseverywhere.Scene;
 import com.transitionseverywhere.Transition;
 import com.transitionseverywhere.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class LayoutActivity extends AppCompatActivity {
 
@@ -25,6 +27,7 @@ public class LayoutActivity extends AppCompatActivity {
     //Флажок для переключения между сценами:
     private boolean start;
 
+
     @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +38,30 @@ public class LayoutActivity extends AppCompatActivity {
         RelativeLayout FL=(RelativeLayout)findViewById(R.id.bg);
 
         //Первая сцена
-        ViewGroup startView=(ViewGroup)getLayoutInflater().inflate(R.layout.layout,FL,false);
+        ViewGroup start1View=(ViewGroup)getLayoutInflater().inflate(R.layout.layout,FL,false);
 
         //Вторая сцена
-        ViewGroup stopView=(ViewGroup)getLayoutInflater().inflate(R.layout.layout2,FL,false);
+        ViewGroup start2View=(ViewGroup)getLayoutInflater().inflate(R.layout.layout2,FL,false);
 
 
         //Создание 2-х сцен
-        scene1=new Scene(FL, startView);
-        scene2=new Scene(FL, stopView);
+        scene1=new Scene(FL, start1View);
+        scene2=new Scene(FL, start2View);
 
         //Настраиваем переход и его свойства:
-        transition = new AutoTransition();
-        //Настраиваем длительность перехода 1 секунд:
+        transition = new ChangeBounds();
+        //Настраиваем длительность перехода 5 секунд:
         transition.setDuration(1000);
         transition.setInterpolator(new AccelerateDecelerateInterpolator());
         //Настраиваем запуск перехода:
-        start=true;
+
+        start = true;
+
 
     }
     // Функция нажатие на кнопки
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    public void changeScene(View view) {
+    public void onClick(View view) {
         if (start) {
             TransitionManager.go(scene2, transition);
             start=false;
@@ -66,4 +71,5 @@ public class LayoutActivity extends AppCompatActivity {
             start=true;
         }
     }
+
 }
